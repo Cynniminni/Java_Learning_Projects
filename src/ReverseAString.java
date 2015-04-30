8b9f7cc00f1af77e3604ead575327f7449f3bce0
Aitalas <radicalcyn@yahoo.com>
2015-04-29 23:54:15 -0700

    reverse a string added

--- src/ReverseAString.java
+++ src/ReverseAString.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
 
+/*
+ * Enter a string and the program will reverse it and print it out.
+ */
 public class ReverseAString {
-
+	public static void main(String[] args) {
+		//initialize
+		String input = "";
+		Stack stack = new Stack<Character>();
+		Scanner scan = new Scanner(System.in);
+		
+		//prompt for string input
+		System.out.println("Please enter a string:");
+		input = scan.nextLine();
+		
+		//push input onto a stack
+		for (int i = 0; i < input.length(); i++) {
+			stack.push(input.charAt(i));
+		}
+		
+		System.out.println("The reversed string is:\n");
+		
+		//print reversed string
+		for (int i = 0; i < input.length(); i++) {
+			System.out.print(stack.pop());
+		}
+	}
 }
